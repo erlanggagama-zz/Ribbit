c826ebd79c2165bf10aaad38c6944f1467edad2f
Erlangga Satria Gama <erlangga.gama@gmail.com>
2015-01-22 18:39:56 +0700

    Adding fragment for Inbox and Friend list.

---
+++ app/src/main/java/erlangga/com/ribbit/InboxFragment.java
@@ -0,0 +1,21 @@
+package erlangga.com.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by erlanggagama on 22/01/15.
+ */
+public class InboxFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        
+        return rootView;
+    }
+}
